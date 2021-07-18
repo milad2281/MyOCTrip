609e0c176a36ffb9e04e3d1c35beff7e3d3c5af7
Milad Mobini <miladmobini.ca@gmail.com>
2021-07-18 11:33:53 -0400

    Added BusRoute activity to android manifest

--- app/src/main/java/algonquin/cst2335/androidproject/busroute/BusRoute.java
+++ app/src/main/java/algonquin/cst2335/androidproject/busroute/BusRoute.java
@@ -13,7 +13,7 @@ public class BusRoute extends AppCompatActivity {
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.br_bus_routes);
 
     }
 }
